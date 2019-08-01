201fc7a2e01b573884a2ef2fdcb0043d603334de
indra aditya <adityain98@gmail.com>
2019-08-01 22:27:50 +0700

    Penambahan exercises-9

---
+++ exercises-9.js
@@ -0,0 +1,35 @@
+// Tugas 1
+
+function shoutOut(){
+    var hello = 'Halo Function!';
+    return hello;   
+}
+
+console.log(shoutOut())
+
+//Tugas 2
+
+function calculateMultiply(a,b){
+    c = a * b;
+    return c;
+}
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
+
+//Tugas 3
+
+function processSentence(e,f,g,h){
+    kata = e +' ' + f +' ' + g +' ' + h;
+    return kata;
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence);
