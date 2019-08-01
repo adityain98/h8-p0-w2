983645264f7321eb609992e0bc9d065ad71f9439
indra aditya <adityain98@gmail.com>
2019-08-01 21:25:22 +0700

    Perbaikan exercises-7

--- exercises-7.js
+++ exercises-7.js
@@ -1,15 +1,20 @@
 // 1. Menyusun Barisan Bintang
 
-for(var rows1=5; rows1<10; rows1++){
+var rows1; // input the number of rows
+
+// do loops to display asterisks in the console.
+
+for(rows1=0; rows1<5; rows1++){
     console.log('*');
 }
 
 // 2. Menyusun Barisan Bintang Dengan Nested Looping
-
+var rows2; // input the number of rows
+// do loops to display asterisks in the console.
 var bintang = '*';
 
-for(var rows2=5; rows2 <10; rows2++){
-    for(var i=1; i<6; i++){
+for(rows2=0; rows2 <5; rows2++){
+    for(i=1; i<6; i++){
         if(i<6){
             bintang = bintang + '*';
         }
@@ -19,10 +24,13 @@ for(var rows2=5; rows2 <10; rows2++){
 }
 
 // 3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+
+var rows3; // input the number of rows
+// do loops to display asterisks in the console.
 var bintang2 = '*';
 
-for(var rows3=5; rows3 <10; rows3++){
-    for(var j=1; j<2; j++){
+for(rows3=0; rows3 <5; rows3++){
+    for(j=1; j<2; j++){
         if(j<6){
             console.log(bintang2)
             bintang2 = bintang2 + '*';
