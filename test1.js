4901cd5f459d0ce336a6be5d82f3ef939a55c7f1
adityain98 <adityain98@gmail.com>
2019-08-04 21:55:53 +0700

    perbaikan final

---
+++ test1.js
@@ -0,0 +1,21 @@
+// var siswa = ['andi', 'elsa', 'randy'];
+// var kukusiswa = [1,1,0]
+// var kukupanjang = 1;
+
+// for(var i= 0 ; i<kukusiswa.length; i++){
+//     if(kukusiswa[i] == kukupanjang){
+//         console.log(siswa[i], 'dihukum');
+//     }
+//     else if(kukusiswa[i] != kukupanjang){
+//         console.log(siswa[i], 'dipuji');
+//     }
+// }
+
+var pakaian = 20;
+
+for(var mesin = 1; mesin<pakaian; mesin++){
+
+}
+
+
+console.log(mesin);
